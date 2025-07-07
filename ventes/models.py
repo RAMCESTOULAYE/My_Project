8db4a6347c7c31915dceaaaba1bcfddacb643886
RamatoulayeNdiaye <rndiaye@lalenacorp.com>
2025-07-07 13:23:46 +0200

    my pull

--- ventes/models.py
+++ ventes/models.py
@@ -4,12 +4,25 @@ from clients.models import Client  # ← importer le bon modèle
 class Interaction(models.Model):
     TYPE_CHOICES = [
         ('vente', 'Vente'),
-        ('appel', 'Appel'),
+        ('achat', 'Achat'),
+        ('appel', 'Appel téléphonique'),
         ('email', 'Email'),
+        ('rdv', 'Rendez-vous'),
+        ('sms', 'SMS'),
+        ('support', 'Support client'),
+        ('devis', 'Devis envoyé'),
+        ('facture', 'Facture envoyée'),
+        ('relance', 'Relance'),
+        ('visite', 'Visite sur site'),
+        ('demande_info', 'Demande d\'information'),
+        ('reclamation', 'Réclamation'),
+        ('remerciement', 'Remerciement'),
+        ('prospection', 'Prospection'),
+        ('autre', 'Autre'),
     ]
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='interactions')
     date = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     commentaire = models.TextField(blank=True)
 
     def __str__(self):
